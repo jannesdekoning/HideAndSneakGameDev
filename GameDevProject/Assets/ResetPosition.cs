9ad0ded7f8ff01f019d39b0e5606a9aba8a20041
Jannes De Koning <jannes.dekoning@student.kdg.be>
2021-02-25 15:20:46 +0100

    added basic boids behavior

---
+++ GameDevProject/Assets/ResetPosition.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject obj in GameObject.FindObjectsOfType<GameObject>())
+        {
+            if (obj.transform.position.x > 25 || obj.transform.position.x < -25)
+            {
+                obj.transform.position = new Vector3(-obj.transform.position.x, obj.transform.position.y, obj.transform.position.z);
+            }
+            if (obj.transform.position.z > 25 || obj.transform.position.z < -25)
+            {
+                obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, -obj.transform.position.z);
+            }
+        }
+    }
+}
