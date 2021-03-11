4354bc99d6a25ec5fded52e3f363e3d740ff1efc
Jannes De Koning <jannes.dekoning@student.kdg.be>
2021-03-11 14:19:27 +0100

    Create
    commit in order to be able to pop the stash (unexpected problems with GitKraken)

---
+++ GameDevProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- GameDevProject/Assets/Scripts/PlayerControls.cs
+++ GameDevProject/Assets/Scripts/PlayerControls.cs
@@ -7,6 +7,10 @@ public class PlayerControls : MonoBehaviour
     private Rigidbody rb;
     public float speed = 5;
 
+    public static bool enableGrow = false;
+    public static bool enableShrink = false;
+    public static bool isTouching = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +21,33 @@ public class PlayerControls : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"))*speed;
+
+        if (enableGrow)
+        {
+            gameObject.transform.localScale += Vector3.one;
+        }
+        else if (enableShrink)
+        {
+            gameObject.transform.localScale -= Vector3.one;
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"{collision.gameObject.name} has entered the collision");
+        if (collision.gameObject.name == "Boid")
+        {
+            isTouching = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log($"{collision.gameObject.name} has exited the collision");
+        if (collision.gameObject.name == "Boid")
+        {
+            isTouching = false;
+        }
     }
 }
